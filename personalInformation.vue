e29909c3e41f52eaacb48fa85ec3becd4ba5c15e
Ruy5 <snake.snakeyy@gmail.com>
2024-07-19 00:45:35 +0800

    修改求职意向

--- personalInformation.vue
+++ personalInformation.vue
@@ -6,7 +6,7 @@
                 <li>年龄: 22</li>
                 <li>手机: 13016703849</li>
                 <li>邮箱: <a href="mailto:snake.snakeyy@gmail.com">snake.snakeyy@gmail.com</a> </li>
-                <li>求职意向：前端开发工程师</li>
+                <li>求职意向：全栈开发、项目管理、技术支持</li>
                 <li>期望薪资：4k-8k</li>
             </ul>
         </div>
